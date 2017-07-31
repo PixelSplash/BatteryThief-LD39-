747c18123bdcf5a57725d4f75b46ff635e974165
Briam Mora <bryam.car@gmail.com>
2017-07-31 17:34:24 -0600

    First commit

---
+++ BatteryThief/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    private GameObject _cam;
+    private ObjectPool _pool;
+    [SerializeField]
+    private GameObject _cop;
+    [SerializeField]
+    private GameObject _powerBall;
+    private float _nextTime;
+    [SerializeField]
+    private float _platformFreq;
+    [SerializeField]
+    private float _creationPoint;
+    private int _platformCount;
+    private float _platformY;
+    private float _rand;
+    [SerializeField]
+    private int _minPathLength;
+    [SerializeField]
+    private int _maxPathLength;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float _pathChangeProb;
+    private float _lastPlatformX;
+    private float _newPlatformX;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float _holeProb;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float _highPathProb;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float _poweBallProb;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float _copFreq;
+
+    // Use this for initialization
+    void Start () {
+        _platformY = 0;
+        _platformCount = 10;
+        _cam = Camera.main.gameObject;
+        _nextTime = Time.time + _platformFreq;
+        _pool = transform.GetComponent<ObjectPool>();
+        _lastPlatformX = _cam.transform.position.x + _creationPoint;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+
+        if (Time.time > _nextTime)
+        {
+            _nextTime = Time.time;
+            _newPlatformX = _cam.transform.position.x + _creationPoint;
+
+            if (_newPlatformX >= _lastPlatformX + 0.32f)
+            {
+                
+
+                
+                _nextTime += +_platformFreq;
+                _lastPlatformX += 0.32f;
+
+                //PowerBall creation
+                _rand = Random.value;
+                if (_rand < _poweBallProb)
+                {
+                    Instantiate(_powerBall, new Vector2(_lastPlatformX, _platformY), Quaternion.identity);
+                }
+
+                //If is hole
+                _rand = Random.value;
+                if (_rand > _holeProb)
+                {
+                    //Cop creation
+                    _rand = Random.value;
+                    if(_rand < _copFreq)
+                    {
+                        Instantiate(_cop, new Vector2(_lastPlatformX, _platformY), Quaternion.identity);
+
+                    }
+                    _pool.GetObject("platform", new Vector2(_lastPlatformX, _platformY), Quaternion.identity);
+                    
+                }
+                _platformCount--;
+
+                if (_platformCount == 0)
+                {
+                    _rand = Random.value;
+                    if (_rand > 1 - _pathChangeProb)
+                    {
+                        _rand = Random.value;
+                        if(_rand < _highPathProb)
+                        {
+                            _platformY += 0.32f;
+                        }
+                        _platformY += 0.32f;
+                    }
+                    else if (_rand < 0 + _pathChangeProb)
+                    {
+                        _platformY -= 0.32f;
+                        if (_platformY < 0) _platformY = 0;
+                    }
+                    _platformCount = (int)Random.Range(0.0f, _maxPathLength - _minPathLength) + _minPathLength;
+                }
+            }
+            
+        }
+	}
+}
---
+++ BatteryThief/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PlayerMovement : MonoBehaviour {
+    public string FileName; // This contains the name of the file. Don't add the ".txt"
+                            // Assign in inspector
+    private TextAsset asset; // Gets assigned through code. Reads the file.
+    private StreamWriter writer; // This is the writer that writes to the file
+    [SerializeField]
+    private TextAsset _scoreFile;
+    [SerializeField]
+    private GameObject _energyBar;
+    private Rigidbody2D _rig;
+    [SerializeField]
+    private float _jumpForce;
+    [SerializeField]
+    private float _velocity;
+    public LayerMask groundLayer;
+    private bool _canBounce;
+    private float _canBounceTime;
+    [SerializeField]
+    private float _canBounceReload;
+    [SerializeField]
+    private int _maxCharge;
+    private int _charge;
+    private float _energyLoseTime;
+    [SerializeField]
+    private float _energyLoseFreq;
+    [SerializeField]
+    private UnityEngine.UI.Text _score;
+    [SerializeField]
+    private UnityEngine.UI.Text _highScore;
+    private bool _canMove;
+    private AudioSource[] _sounds;
+    private Animator _animator;
+    [SerializeField]
+    private ParticleSystem _particles;
+
+    // Use this for initialization
+    void Start () {
+        _animator = transform.GetComponent<Animator>();
+        _sounds = transform.GetComponents<AudioSource>();
+        _charge = _maxCharge;
+        _rig = transform.GetComponent<Rigidbody2D>();
+        _canBounce = true;
+        _energyLoseTime = Time.time + _energyLoseFreq;
+        _highScore.text = System.IO.File.ReadAllText("Assets/Resources/" + FileName + ".txt"); ;
+        _canMove = true;
+        _rig.velocity = new Vector2(_velocity, _rig.velocity.y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //Dead
+        if (_charge <= 0 || transform.position.y < -0.17f)
+        {
+            Die();
+        }
+
+        //Setting score UI
+        _score.text = ""+(int)transform.position.x;
+
+        //Running out of power
+        if (Time.time > _energyLoseTime)
+        {
+            if (_charge > 0)
+            {
+                _sounds[4].Play();
+                _charge--;
+                _energyBar.transform.localScale = new Vector2(_energyBar.transform.localScale.x, _charge);
+                
+            }
+            _energyLoseTime = Time.time + _energyLoseFreq;
+        }
+
+        //constant velocity
+        _rig.AddForce(Vector2.right * _velocity * 4);
+        if(_rig.velocity.x > _velocity) _rig.velocity = new Vector2(_velocity, _rig.velocity.y);
+
+
+        //Inputs
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //_highScore.text = _scoreFile.text;
+            if (IsGrounded())
+            {
+                _sounds[1].Play();
+                _rig.AddForce(Vector2.up * _jumpForce);
+            }
+           
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(_charge > 0)
+            {
+                _particles.Play();
+                ParticleSystem.EmissionModule em = _particles.emission;
+                em.enabled = true;
+                _sounds[2].Play();
+                _charge--;
+                _energyBar.transform.localScale = new Vector2(_energyBar.transform.localScale.x, _charge);
+                _rig.AddForce(Vector2.up * _jumpForce * 1.5f);
+                _canMove = true;
+            }
+            
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+
+        //Audio
+        if (IsGrounded())
+        {
+            if(!_sounds[0].isPlaying) _sounds[0].Play();
+        }
+        else
+        {
+            if (_sounds[0].isPlaying) _sounds[0].Stop();
+        }
+
+        //animation
+        _animator.SetBool("isGrounded", IsGrounded());
+        _animator.SetFloat("yVel", _rig.velocity.y);
+
+    }
+
+
+
+    bool IsGrounded()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 0.4f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    bool IsStucked()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.right;
+        float distance = 0.4f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "PowerUp")
+        {
+            _sounds[3].Play();
+            if (_charge < _maxCharge)
+            {
+                
+                _charge++;
+                _energyBar.transform.localScale = new Vector2(_energyBar.transform.localScale.x,_charge);
+            }
+            Destroy(other.gameObject);
+        }else if (other.tag == "Danger")
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //Debug.Log((int)transform.position.x);
+        //Debug.Log(IntParseFast(_scoreFile.text));
+        
+        if (IntParseFast(_highScore.text) < (int)transform.position.x)
+        {
+            
+            AppendString(((int)transform.position.x).ToString());
+        }
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    public static int IntParseFast(string value)
+    {
+        int result = 0;
+        for (int i = 0; i < value.Length - 2; i++)
+        {
+           
+            char letter = value[i];
+
+            result = 10 * result + (letter - 48);
+            //Debug.Log(letter);
+            //Debug.Log(result);
+        }
+        return result;
+    }
+
+    void AppendString(string appendString)
+    {
+        asset = Resources.Load(FileName + ".txt") as TextAsset;
+        writer = new StreamWriter("Assets/Resources/" + FileName + ".txt"); // Does this work?
+        writer.WriteLine(appendString);
+        writer.Close();
+    }
+}
